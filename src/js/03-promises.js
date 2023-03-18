3ab825b6a21486c842ab56c4948e43a2e1a4f96b
HalynaSilina <halynasilina@gmail.com>
2023-03-18 11:24:07 +0200

    change css

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+const createPromiseForm = document.querySelector('.form');
+
+createPromiseForm.addEventListener('submit', handleCreatePromiseFormSubmit);
+
+function handleCreatePromiseFormSubmit(e) {
+e.preventDefault();
+const {
+  elements: { delay, step, amount }
+} = e.currentTarget;
+console.log(`delay: ${delay.value}, step: ${step.value}, amount: ${amount.value}`);
+e.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
