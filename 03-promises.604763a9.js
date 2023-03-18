d02db1a5c44d4ebd1d8af31641dc4031227cfb96
HalynaSilina <113549968+HalynaSilina@users.noreply.github.com>
2023-03-18 10:42:05 +0000

    Deploying to gh-pages from @ HalynaSilina/goit-js-hw-09@153d4474f1c72aa088e8f5ce6d3a1fd1044b40d2 🚀

--- 03-promises.604763a9.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements,a=t.delay,n=t.step,r=t.amount;console.log("delay: ".concat(a.value,", step: ").concat(n.value,", amount: ").concat(r.value)),e.currentTarget.reset()}));
-//# sourceMappingURL=03-promises.604763a9.js.map
